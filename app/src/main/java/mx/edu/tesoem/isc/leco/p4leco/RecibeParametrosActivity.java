65f59f6c6b643222c45484bde59415285b358e3c
Luis Enrique Carreon Ortega <enr900@live.com.mx>
2019-10-27 17:07:32 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/leco/p4leco/RecibeParametrosActivity.java
@@ -0,0 +1,21 @@
+package mx.edu.tesoem.isc.leco.p4leco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RecibeParametrosActivity extends AppCompatActivity {
+TextView vnombre;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recibe_parametros);
+        Bundle parametros = getIntent().getExtras();
+        vnombre = (TextView)findViewById(R.id.lblparametros);
+        vnombre.setText("Bienvenido  "+parametros.getString("pnombre"));
+
+    }
+}
